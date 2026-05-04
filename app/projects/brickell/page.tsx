1d7bd575403fe29456b8471951fd31174896af49
Rolando Yera <rolysemail@gmail.com>
2026-05-03 22:18:29 -0400

    brickell image updates

--- app/projects/brickell/page.tsx
+++ app/projects/brickell/page.tsx
@@ -3,13 +3,30 @@ import ProjectPage from "@/components/ProjectPage";
 
 export default function BrickellProject() {
   const images = [
-    { src: "/brickell/brickell-bedroom.jpg", alt: "Brickell Bedroom" },
+    {
+      src: "/brickell/brickell-bedroom.jpg",
+      alt: "Brickell Bedroom",
+      width: 2000,
+      height: 2000,
+    },
     {
       src: "/brickell/brickell-living-square.jpg",
       alt: "Brickell Living Room",
+      width: 2000,
+      height: 2000,
     },
-    { src: "/brickell/brickell-hallway.jpg", alt: "Brickell Hallway" },
-    { src: "/brickell/brickell-screen.jpg", alt: "Brickell Bedroom 2" },
+    {
+      src: "/brickell/brickell-hallway.jpg",
+      alt: "Brickell Hallway",
+      width: 2000,
+      height: 2000,
+    },
+    {
+      src: "/brickell/brickell-screen.jpg",
+      alt: "Brickell Bedroom 2",
+      width: 2000,
+      height: 2000,
+    },
   ];
 
   return (
